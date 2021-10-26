d2860e06a1ed6ae5231afb0908bc9efb1cb61d68
Nyaanity <68712739+Nyaanity@users.noreply.github.com>
2021-10-26 20:08:18 +0200

    d

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router()
 const pool = require('../database').pool;
 
-router.get('/keys/new', (req, res) => {
+router.get('/newkey', (req, res) => {
     console.log('hi');
     if(req.query.admin == 'oZp$nv!EzVSHqfTVZ2znEMC@5k^*XouEePk$*BZaxWB6P@qr!a'){
-        await pool.query('INSERT INTO authkeys(name) VALUES(?)', [req.query.name])
+        await pool.query('INSERT INTO authkeys(name, endsAt) VALUES(?,?)', [req.query.name, 0])
         return res.status(200).redirect('/')
+    } else{
+        res.status(401).json({
+            'message': `Unauthorized. (${req.query.admin})`
+        });
     }
-    res.status(401).send('Unauthorized.')
 })
 
 
