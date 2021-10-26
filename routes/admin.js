f77e1f24db567749c90c0f19fdb159bf7f30330b
Nyaanity <68712739+Nyaanity@users.noreply.github.com>
2021-10-26 19:27:40 +0200

    .

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const pool = require('../database').pool;
 
 router.get('/keys/new', (req, res) => {
     console.log(req.query)
-    if(req.query.admin != 'oZp$nv!EzVSHqfTVZ2znEMC@5k^*XouEePk$*BZaxWB6P@qr!a'){
+    if(req.query.admin == 'oZp$nv!EzVSHqfTVZ2znEMC@5k^*XouEePk$*BZaxWB6P@qr!a'){
         await pool.query('INSERT INTO authkeys(name) VALUES(?)', [req.query.name])
         res.status(200).redirect('/')
         return
