73e23f02d65be637ce71d126ab72778c42940669
Nyaanity <68712739+Nyaanity@users.noreply.github.com>
2021-11-05 11:36:55 +0100

    db external

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config({ path: './.env' });
 const pool = mariadb.createPool({
     'host': process.env.DB_HOST,
     'port': process.env.DB_PORT,
-    'utser': process.env.DB_USER,
+    'user': process.env.DB_USER,
     'password': process.env.DB_PASSWORD,
     'database': process.env.DB_DATABASE,
     'connectionLimit': process.env.DB_CONNECTION_LIMIT
